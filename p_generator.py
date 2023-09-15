230b25eab30e21a39e3fc1d7abb0a333a19cb92a
Camty1 <cameron.j.wolfe.23@dartmouth.edu>
2023-09-15 14:11:35 -0400

    added comments and refactored

--- p_generator.py
+++ p_generator.py
@@ -1,25 +1,35 @@
-n = 100
+# Define parameters of game
+n = 46
 G = set(range(n+1))
-S = {1,2,4,8,16,32,64}
+S = {1,3,6}
 
+# Initialize P and N sets
 P = set()
 N = set()
 
+# Go through until every position in G has been added to either P or N
 while G:
+    # Put the smallest value left in G into P, as this can only go to N, as it would have otherwise been put in N by smaller P values
     P = P.union({min(G)})
-
+    
+    # For all values p in P, there exists a value n in N and a value s in S such that n - s = p
     for s in S:
         for p in P:
             value = p + s
             if value <= n:
                 N = N.union({p + s})
-
+    
+    # Remove values in P and N from G
     G = G - P - N
 
-print(P)
-print(N)
+# Print P and N sets
+print("P set: ")
+print(sorted(P))
+print("N set: ")
+print(sorted(N))
 
+# Determine whether to play first or second based on whether n (the number of coins) is in P or N
 if n in P:
-    print("Second")
+    print("Play second to win.")
 else:
-    print("First")
+    print("Play first to win.")
